9ddacaa9d5d82ee814a64053e76963c4a3606291
Kai Armer <kai_armer@hotmail.com>
2020-05-09 16:40:29 +0100

    Adds parameterized tests.

--- src/test/kotlin/ConverterTester.kt
+++ src/test/kotlin/ConverterTester.kt
@@ -1,10 +1,13 @@
 import org.junit.jupiter.api.Assertions.assertEquals
-import org.junit.jupiter.api.Test
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.CsvSource
 
 class ConverterTester {
 
-    @Test
-    fun testConverter_givenNumber_convertsToRomanNumerals() {
-        assertEquals("I", Converter().convert(1))
+    @ParameterizedTest
+    @CsvSource("1, I")
+    fun `convert number into roman numeral`(input: Int, output: String) {
+        val result = Converter().convert(input)
+        assertEquals(output, result)
     }
 }
