b7ca9cb700b85592fbf28f3ec9cfb06b93dc65ad
Kai Armer <kai_armer@hotmail.com>
2020-05-09 19:19:32 +0100

    A little more refactoring - top down

--- src/main/kotlin/Converter.kt
+++ src/main/kotlin/Converter.kt
@@ -3,6 +3,41 @@ class Converter {
         return convertOnes(input)
     }
 
+    private fun convertOnes(number: Int): String {
+        val lowerNumeral = nextLowestNumeral(number) ?: Numeral.ONE
+        val higherNumeral = nextHighestNumeral(number) ?: Numeral.ONE_HUNDRED
+        val matchingNumeral = matchingRomanNumeral(number)
+
+        if (matchingNumeral != null) return matchingNumeral.numeral
+        if (shouldSubtractFromNextHighestNumeral(number, higherNumeral.value)) return one() + higherNumeral.numeral
+        return convertToNumeralsWithAddition(lowerNumeral, number)
+    }
+
+    private fun nextLowestNumeral(number: Int): Numeral? {
+        val numerals = Numeral.values()
+        numerals.reverse()
+        return numerals.firstOrNull { numeral -> number > numeral.value }
+    }
+
+    private fun nextHighestNumeral(number: Int): Numeral? {
+        return Numeral.values().firstOrNull { numeral -> number < numeral.value }
+    }
+
+    private fun matchingRomanNumeral(number: Int): Numeral? {
+        return Numeral.values().firstOrNull { numeral -> numeral.value == number }
+    }
+
+    private fun shouldSubtractFromNextHighestNumeral(number: Int, nextHighestNumeralValue: Int): Boolean {
+        return nextHighestNumeralValue - number == 1
+    }
+
+    private fun convertToNumeralsWithAddition(lowerNumeral: Numeral, number: Int): String {
+        var output = lowerNumeral.numeral
+        val difference = number - lowerNumeral.value
+        output += times(difference) { one() }
+        return output
+    }
+
     private fun times(times: Int = 1, completion: () -> String): String {
         var output = ""
         for (i in 1..times) {
@@ -21,40 +56,5 @@ class Converter {
 
     private fun ten(): String {
         return Numeral.TEN.numeral
-    }
-
-    private fun convertOnes(number: Int): String {
-        val lowerNumeral = nextLowestNumeral(number) ?: Numeral.ONE
-        val higherNumeral = nextHighestNumeral(number) ?: Numeral.ONE_HUNDRED
-        val matchingNumeral = matchingRomanNumeral(number)
-
-        if (matchingNumeral != null) return matchingNumeral.numeral
-        if (shouldSubtractFromNextHighestNumeral(number, higherNumeral.value)) return one() + higherNumeral.numeral
-        return convertToNumeralsWithAddition(lowerNumeral, number)
-    }
-
-    private fun convertToNumeralsWithAddition(lowerNumeral: Numeral, number: Int): String {
-        var output = lowerNumeral.numeral
-        val difference = number - lowerNumeral.value
-        output += times(difference) { one() }
-        return output
-    }
-
-    private fun matchingRomanNumeral(number: Int): Numeral? {
-        return Numeral.values().firstOrNull { numeral -> numeral.value == number }
-    }
-
-    private fun shouldSubtractFromNextHighestNumeral(number: Int, nextHighestNumeralValue: Int): Boolean {
-        return nextHighestNumeralValue - number == 1
-    }
-
-    private fun nextHighestNumeral(number: Int): Numeral? {
-        return Numeral.values().firstOrNull { numeral -> number < numeral.value }
-    }
-
-    private fun nextLowestNumeral(number: Int): Numeral? {
-        val numerals = Numeral.values()
-        numerals.reverse()
-        return numerals.firstOrNull { numeral -> number > numeral.value }
     }
 }
