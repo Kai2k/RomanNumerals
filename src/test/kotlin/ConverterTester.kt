7f898635e685236a27db4cb082d9aa7f2dcfbfb6
Kai Armer <kai_armer@hotmail.com>
2020-05-09 19:23:44 +0100

    Added tests for 11-20

--- src/test/kotlin/ConverterTester.kt
+++ src/test/kotlin/ConverterTester.kt
@@ -6,7 +6,14 @@ class ConverterTester {
 
     @ParameterizedTest
     @CsvSource("1, I", "2, II", "3, III", "4, IV", "5, V", "6, VI", "7, VII", "8, VIII", "9, IX", "10, X")
-    fun `convert number into roman numeral`(input: Int, output: String) {
+    fun `convert numbers 1 - 10 into roman numeral`(input: Int, output: String) {
+        val result = Converter().convert(input)
+        assertEquals(output, result)
+    }
+
+    @ParameterizedTest
+    @CsvSource("11, XI", "12, XII", "13, XIII", "14, XIV", "15, XV", "16, XVI", "17, XVII", "18, XVIII", "19, XIX", "20, XX")
+    fun `convert numbers 11 - 20 into roman numeral`(input: Int, output: String) {
         val result = Converter().convert(input)
         assertEquals(output, result)
     }
